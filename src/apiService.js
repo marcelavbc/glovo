d38ba679302cebc10201e77afd5aef28eeb74846
marcela <marcela.vilasboas@gmail.com>
2021-04-28 10:45:05 +0200

    fix error response from api

--- src/apiService.js
+++ src/apiService.js
@@ -1,25 +1,35 @@
 const axios = require('axios');
-const { Cocktail } = require('./cocktail')
+const { Cocktail } = require('./cocktail');
 
-module.exports = { 
+module.exports = {
     filterCocktailsByLetter(letter) {
         return new Promise((resolve, reject) => {
-            axios({ url: `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`, method: 'GET' })
-                .then(resp => {
-                    const drinks = resp.data.drinks;
-                    let cocktails = [];
-
-                    for (let i = 0; i < drinks.length; i++) {
-                        cocktails[i] = new Cocktail(drinks[i].idDrink, drinks[i].strDrink, drinks[i].strAlcoholic);
-                        cocktails[i].getIngredients([drinks[i].strIngredient1, drinks[i].strIngredient2, drinks[i].strIngredient3, drinks[i].strIngredient4, drinks[i].strIngredient5, drinks[i].strIngredient6, drinks[i].strIngredient7, drinks[i].strIngredient8, drinks[i].strIngredient9, drinks[i].strIngredient10, drinks[i].strIngredient11, drinks[i].strIngredient12, drinks[i].strIngredient13, drinks[i].strIngredient14, drinks[i].strIngredient15]);
-                        cocktails[i].getIngredientsAndQtt([{ ing: drinks[i].strIngredient1, qtt: drinks[i].strMeasure1 }, { ing: drinks[i].strIngredient2, qtt: drinks[i].strMeasure2 }, { ing: drinks[i].strIngredient3, qtt: drinks[i].strMeasure3 }, { ing: drinks[i].strIngredient4, qtt: drinks[i].strMeasure4 }, { ing: drinks[i].strIngredient5, qtt: drinks[i].strMeasure5 }, { ing: drinks[i].strIngredient6, qtt: drinks[i].strMeasure6 }, { ing: drinks[i].strIngredient7, qtt: drinks[i].strMeasure7 }, { ing: drinks[i].strIngredient8, qtt: drinks[i].strMeasure8 }, { ing: drinks[i].strIngredient9, qtt: drinks[i].strMeasure9 }, { ing: drinks[i].strIngredient10, qtt: drinks[i].strMeasure10 }, { ing: drinks[i].strIngredient11, qtt: drinks[i].strMeasure11 }, { ing: drinks[i].strIngredient12, qtt: drinks[i].strMeasure12 }, { ing: drinks[i].strIngredient13, qtt: drinks[i].strMeasure13 }, { ing: drinks[i].strIngredient14, qtt: drinks[i].strMeasure14 }, { ing: drinks[i].strIngredient15, qtt: drinks[i].strMeasure15 }]);
-                    }
-                    if (resp) {
+            try {
+                axios({ url: `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${letter}`, method: 'GET' })
+                    .then(resp => {
+                        const drinks = resp.data.drinks;
+                        let cocktails = [];
+                        for (let i = 0; i < drinks.length; i++) {
+                            cocktails[i] = new Cocktail(drinks[i].idDrink, drinks[i].strDrink, drinks[i].strAlcoholic);
+                            cocktails[i].getIngredients([drinks[i].strIngredient1, drinks[i].strIngredient2, drinks[i].strIngredient3, drinks[i].strIngredient4, drinks[i].strIngredient5, drinks[i].strIngredient6, drinks[i].strIngredient7, drinks[i].strIngredient8, drinks[i].strIngredient9, drinks[i].strIngredient10, drinks[i].strIngredient11, drinks[i].strIngredient12, drinks[i].strIngredient13, drinks[i].strIngredient14, drinks[i].strIngredient15]);
+                            cocktails[i].getIngredientsAndQtt([{ ing: drinks[i].strIngredient1, qtt: drinks[i].strMeasure1 }, { ing: drinks[i].strIngredient2, qtt: drinks[i].strMeasure2 }, { ing: drinks[i].strIngredient3, qtt: drinks[i].strMeasure3 }, { ing: drinks[i].strIngredient4, qtt: drinks[i].strMeasure4 }, { ing: drinks[i].strIngredient5, qtt: drinks[i].strMeasure5 }, { ing: drinks[i].strIngredient6, qtt: drinks[i].strMeasure6 }, { ing: drinks[i].strIngredient7, qtt: drinks[i].strMeasure7 }, { ing: drinks[i].strIngredient8, qtt: drinks[i].strMeasure8 }, { ing: drinks[i].strIngredient9, qtt: drinks[i].strMeasure9 }, { ing: drinks[i].strIngredient10, qtt: drinks[i].strMeasure10 }, { ing: drinks[i].strIngredient11, qtt: drinks[i].strMeasure11 }, { ing: drinks[i].strIngredient12, qtt: drinks[i].strMeasure12 }, { ing: drinks[i].strIngredient13, qtt: drinks[i].strMeasure13 }, { ing: drinks[i].strIngredient14, qtt: drinks[i].strMeasure14 }, { ing: drinks[i].strIngredient15, qtt: drinks[i].strMeasure15 }]);
+                        }
                         resolve(cocktails)
-                    } else {
-                        reject(error => console.log('Error:', error))
-                    }
-                })
-        })
-    },
+                    })
+                    .catch(function (error) {
+                        if (error.response) {
+                            console.log('Error:', error.response.status);
+                        } else if (error.request) {
+                            // The request was made but no response was received
+                            console.log('Error:', error.request);
+                        } else {
+                            // Something happened in setting up the request that triggered an Error
+                            console.log('Error:', error.message);
+                        }
+                    })
+            } catch (error) {
+                console.log(error)
+            }
+        })   
+    }
 }
